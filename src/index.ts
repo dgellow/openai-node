bd39ee11fc347e139ca52c92a44015eb6398815c
Samuel El-Borai <sam@elborai.me>
2023-08-24 19:50:26 +0200

    feat: doc change

--- src/index.ts
+++ src/index.ts
@@ -19,6 +19,8 @@ export interface ClientOptions {
   baseURL?: string;
 
   /**
+   * HERE IS A CHANGE!
+   *
    * The maximum amount of time (in milliseconds) that the client should wait for a response
    * from the server before timing out a single request.
    *
